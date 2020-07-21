b25670ed4c14263d3a3f50820b471a298003e1b6
samyoo-zhang <804388774@qq.com>
2020-07-22 02:36:17 +0800

    feat: 阿拉伯数字转中文

--- frontend/javascript/functions/utils.js
+++ frontend/javascript/functions/utils.js
@@ -122,3 +122,42 @@ export function toTop(step, time) {
     clearTimeout(timer);
   }
 }
+
+
+/**
+ * 阿拉伯数字转中文数字
+ * @param {number|string} str 
+ */
+export function numberToChinese(str) {
+  let num = parseFloat(str);
+  let strOutput = '',
+    strUnit = '仟佰拾亿仟佰拾万仟佰拾元角分';
+  num += '00';
+  let intPos = num.indexOf('.');
+  if (intPos >= 0) {
+    num = num.substring(0, intPos) + num.substr(intPos + 1, 2);
+  }
+  strUnit = strUnit.substr(strUnit.length - num.length);
+  for (let i = 0; i < num.length; i++) {
+    strOutput +=
+      '零壹贰叁肆伍陆柒捌玖'.substr(num.substr(i, 1), 1) +
+      strUnit.substr(i, 1);
+  }
+  return strOutput
+    .replace(/零角零分$/, '')
+    .replace(/零[仟佰拾]/g, '零')
+    .replace(/零{2,}/g, '零')
+    .replace(/零([亿|万])/g, '$1')
+    .replace(/零+元/, '元')
+    .replace(/亿零{0,3}万/, '亿')
+    .replace(/^元/, '零元');
+}
+
+/**
+ * 去除文本中的html
+ * @param {string} str 
+ */
+export function cancelMark(str) {
+  const reg = /<[a-z0-9]*?>/ig;
+  str.replace(reg, '');
+}
